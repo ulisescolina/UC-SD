a2b8762bb4fe380c85660154b24952f7a60579d9
Ulises <uli.r19@gmail.com>
2018-04-12 22:02:08 -0300

    Comunicacion de procesos funcional, agrego Listener y Sender

--- Proyecto 1/Proceso/Proceso/src/proceso/Proceso.java
+++ Proyecto 1/Proceso/Proceso/src/proceso/Proceso.java
@@ -5,6 +5,11 @@
  */
 package proceso;
 
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author netbeans
@@ -14,8 +19,30 @@ public class Proceso {
     /**
      * @param args the command line arguments
      */
+    private static String[][] hoststxt = {{"localhost", "7001"},{"localhost", "7002"},{"localhost", "7003"}};
+    private static String[] server = {"localhost", "7000"};
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            int port = 7002;
+            Listener l = new Listener();
+            l.start();
+            
+            Socket s = new Socket("localhost", 7000);
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            dos.writeUTF("UP"+ String.valueOf(port));
+            
+            //ServerSocket ss = new ServerSocket(Integer.valueOf(args[0]));
+            ServerSocket ss = new ServerSocket(port);
+            Socket sEscucha = ss.accept();
+            DataInputStream dis = new DataInputStream(sEscucha.getInputStream());
+            String sign = dis.readUTF();
+            System.out.println(sign);
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Proceso.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
