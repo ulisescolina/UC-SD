adcb1295d3e8601b0c89cf949ea648b41c94d06d
Ulises <ulisescolrez@gmail.com>
2018-05-31 22:04:15 -0300

    Renombro proyecto, ahora este actua como cliente

--- Proyecto 4/Urano/src/uranoc/UranoC.java
+++ Proyecto 4/Urano/src/uranoc/UranoC.java
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package urano;
+package uranoc;
 
 /**
  *
  * @author urc
  */
-public class Urano {
+public class UranoC {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
